d7b576807e217af71af116621ef252c81806a9ea
MakarKs <makario270109agar@gmail.com>
2023-03-21 18:05:26 +0300

    second commit

---
+++ Series.java
@@ -0,0 +1,11 @@
+storage 22;
+ Public class main{
+ public static void main(string[] args){
+   Scanner scanner = new scanner(System.in)
+           scanner a = 12;
+   int a = scanner.nextInt();
+   res = 0
+for (int i = 0; i <= a; i++);
+  res += i;
+ }
+} 
